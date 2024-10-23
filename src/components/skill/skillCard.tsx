5b5bd045f13f28c140d1e1b8581aa2b4dbbe593b
qiu <qiujiaxp@163.com>
2024-10-23 15:23:38 +0800

    feat: optimize module code

--- src/components/skill/skillCard.tsx
+++ src/components/skill/skillCard.tsx
@@ -1,71 +1,31 @@
 'use client';
 
 import React from 'react';
-import { Card, Dropdown, Menu } from 'antd';
-import { useRouter } from 'next/navigation';
-import Icon from '@/components/icon';
-import Image from 'next/image';
+import EntityCard from '@/components/entity-card';
 import { Skill } from '@/types/skill';
-import styles from '@/styles/common.less';
-import { useTranslation } from '@/utils/i18n';
-
-const { Meta } = Card;
 
 interface SkillCardProps extends Skill {
   index: number;
   onMenuClick: (action: string, skill: Skill) => void;
 }
 
-const SkillCard: React.FC<SkillCardProps> = ({ id, name, introduction, created_by, team_name, team, index, onMenuClick }) => {
-  const router = useRouter();
-  const { t } = useTranslation();
-
-  const menu = (
-    <Menu>
-      <Menu.Item key={`edit-${id}`} onClick={() => onMenuClick('edit', { id, name, introduction, created_by, team_name, team })}>
-        { t('common.edit') }
-      </Menu.Item>
-      <Menu.Item key={`delete-${id}`} onClick={() => onMenuClick('delete', { id, name, introduction, created_by, team_name, team })}>
-        { t('common.delete') }
-      </Menu.Item>
-    </Menu>
-  );
-
-  const iconType = index % 2 === 0 ? 'jishuqianyan' : 'theory';
-  const avatar = index % 2 === 0 ? '/banner_bg_1.jpg' : '/banner_bg_2.jpg';
+const SkillCard: React.FC<SkillCardProps> = (props) => {
+  const { id, name, introduction, created_by, team_name, team, index, onMenuClick } = props;
+  const iconTypeMapping: [string, string] = ['jishuqianyan', 'theory'];
 
   return (
-    <Card 
-      className={`shadow-md cursor-pointer rounded-xl relative overflow-hidden ${styles.CommonCard}`} 
-      onClick={() => router.push(`/skill/settings?id=${id}&name=${name}&desc=${introduction}`)}>
-      <div className="absolute top-2 right-2 z-10" onClick={(e) => e.stopPropagation()}>
-        <Dropdown overlay={menu} trigger={['click']} key={`dropdown-${id}`} placement="bottomRight">
-          <div className="cursor-pointer">
-            <Icon type="sangedian-copy" className="text-xl" />
-          </div>
-        </Dropdown>
-      </div>
-      <div className="w-full h-[60px] relative">
-        <Image alt="avatar" src={avatar} layout="fill" objectFit="cover" className="rounded-t-xl" />
-      </div>
-      <div className={`w-16 h-16 rounded-full flex justify-center items-center ${styles.iconContainer}`}>
-        <Icon type={iconType} className="text-5xl" />
-      </div>
-      <div className="p-4">
-        <Meta
-          title={name}
-          description={
-            <>
-              <p className={`my-5 text-sm line-clamp-3 h-[60px] ${styles.desc}`}>{introduction}</p>
-              <div className={`absolute bottom-4 right-4 text-xs ${styles.desc}`}>
-                <span className="pr-5">{t('skill.form.owner')}: {created_by}</span>
-                <span>{t('skill.form.group')}: {Array.isArray(team_name) ? team_name.join(',') : '--'}</span>
-              </div>
-            </>
-          }
-        />
-      </div>
-    </Card>
+    <EntityCard 
+      id={id} 
+      name={name}
+      introduction={introduction}
+      created_by={created_by}
+      team_name={team_name}
+      team={team}
+      index={index}
+      onMenuClick={onMenuClick}
+      redirectUrl="/skill/settings"
+      iconTypeMapping={iconTypeMapping}
+    />
   );
 };
 
--- src/components/studio/studioCard.tsx
+++ src/components/studio/studioCard.tsx
@@ -1,78 +1,32 @@
-import React from 'react';
-import { Card, Dropdown, Menu, Tag } from 'antd';
-import { useRouter } from 'next/navigation';
-import Icon from '@/components/icon';
-import Image from 'next/image';
-import { Studio } from '@/types/studio';
-import styles from '@/styles/common.less';
-import studioStyles from './index.module.less';
-import { useTranslation } from '@/utils/i18n';
+'use client';
 
-const { Meta } = Card;
+import React from 'react';
+import EntityCard from '@/components/entity-card';
+import { Studio } from '@/types/studio';
 
 interface StudioCardProps extends Studio {
   index: number;
   onMenuClick: (action: string, studio: Studio) => void;
 }
 
-const StudioCard: React.FC<StudioCardProps> = ({ id, name, introduction, created_by, team, team_name, index, online, onMenuClick }) => {
-  const router = useRouter();
-  const { t } = useTranslation();
-
-  const menu = (
-    <Menu>
-      <Menu.Item key={`edit-${id}`} onClick={() => onMenuClick('edit', { id, name, introduction, created_by, team_name, team, online })}>
-        {t('common.edit')}
-      </Menu.Item>
-      <Menu.Item key={`delete-${id}`} onClick={() => onMenuClick('delete', { id, name, introduction, created_by, team_name, team, online })}>
-        {t('common.delete')}
-      </Menu.Item>
-    </Menu>
-  );
-
-  const iconType = index % 2 === 0 ? 'jiqirenjiaohukapian' : 'jiqiren';
-  const avatar = index % 2 === 0 ? '/banner_bg_1.jpg' : '/banner_bg_2.jpg';
+const StudioCard: React.FC<StudioCardProps> = (props) => {
+  const { id, name, introduction, created_by, team_name, team, index, online, onMenuClick } = props;
+  const iconTypeMapping: [string, string] = ['jiqirenjiaohukapian', 'jiqiren'];
 
   return (
-    <Card 
-      className={`shadow-md cursor-pointer rounded-xl relative overflow-hidden ${styles.CommonCard}`} 
-      onClick={() => router.push(`/studio/detail/settings?id=${id}&name=${name}&desc=${introduction}`)}
-    >
-      <div className="absolute top-2 right-2 z-10" onClick={(e) => e.stopPropagation()}>
-        <Dropdown overlay={menu} trigger={['click']} key={`dropdown-${id}`} placement="bottomRight">
-          <div className="cursor-pointer">
-            <Icon type="sangedian-copy" className="text-xl"/>
-          </div>
-        </Dropdown>
-      </div>
-      <div className="w-full h-[60px] relative">
-        <Image alt="avatar" src={avatar} layout="fill" objectFit="cover" className="rounded-t-xl" />
-      </div>
-      <div className={`w-16 h-16 rounded-full flex justify-center items-center ${styles.iconContainer}`}>
-        <Icon type={iconType} className="text-5xl" />
-      </div>
-      <div className="p-4 relative">
-        <Meta
-          title={name}
-          description={
-            <>
-              <p className={`my-5 text-sm line-clamp-3 h-[60px] ${styles.desc}`}>{introduction}</p>
-              <div className={`flex items-center justify-between ${studioStyles.footer}`}>
-                <Tag
-                  color={online ? 'green' : ''}
-                  className={`${styles.statusTag} ${online ? styles.online : styles.offline}`}>
-                  {online ? t('studio.on') : t('studio.off')}
-                </Tag>
-                <div className={studioStyles.info}>
-                  <span className="pr-5">{t('skill.form.owner')}: {created_by}</span>
-                  <span>{t('skill.form.group')}: {Array.isArray(team_name) ? team_name.join(',') : '--'}</span>
-                </div>
-              </div>
-            </>
-          }
-        />
-      </div>
-    </Card>
+    <EntityCard 
+      id={id} 
+      name={name}
+      introduction={introduction}
+      created_by={created_by}
+      team_name={team_name}
+      team={team}
+      index={index}
+      online={online}
+      onMenuClick={onMenuClick}
+      redirectUrl="/studio/detail/settings"
+      iconTypeMapping={iconTypeMapping}
+    />
   );
 };
 
